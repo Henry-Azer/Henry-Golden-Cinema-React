ea37e0d9553cd3da0fe4f726433a4bca6c3b8a61
Henry-Azer <henaryazer@outlook.com>
2021-12-18 12:19:33 +0200

    adding chatbot

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./components/home";
-import ChatBot from "./components/chatbot";
+import GoldenChatBot from "./components/chatbot";
 
 const routes = () => {
     return (
         <Routes>
             <Route exact path="/" element={<Home />} />
-            <Route exact path="/chatbot" element={<ChatBot />} />
+            <Route exact path="/chatbot" element={<GoldenChatBot />} />
         </Routes>
     );
 };
