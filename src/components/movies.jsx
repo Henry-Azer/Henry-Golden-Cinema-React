d31056d40d2f3b837887c91d07a33ffd74da28a0
Henry-Azer <henaryazer@outlook.com>
2021-12-31 13:22:23 +0200

    Finish Movies Route

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+import Typography from "@mui/material/Typography";
+
+import { FaCaretRight } from "react-icons/fa";
+
 import Header from "./global/header";
 import Footer from "./global/footer";
 import Chatbot from "./global/chatbot";
@@ -9,7 +13,158 @@ const Movies = () => {
         <section className="movies-route">
             <Header />
             <Chatbot />
-            <div className="home-wrapper content-fit"></div>
+            <div className="movies-wrapper content-fit">
+                <div className="heading-wrapper display-flex flex-row">
+                    <FaCaretRight className="heading-icon" />
+                    <Typography
+                        className="heading-title"
+                        variant="h4"
+                        component="div"
+                    >
+                        <b>Now Playing</b>
+                    </Typography>
+                </div>
+                <div className="movies-cards-wrapper display-flex flex-row">
+                    <div className="movie-card">
+                        <img
+                            className="movie-card-img"
+                            src="https://assets.voxcinemas.com/posters/P_HO00006784.jpg"
+                            alt="movie-img"
+                        />
+                        <div className="movie-card-info">
+                            <div className="rate-wrapper display-flex flex-row">
+                                <Typography
+                                    className="rate-number"
+                                    variant="subtitle1"
+                                    component="div"
+                                >
+                                    8.3/10
+                                </Typography>
+                                <img
+                                    className="imdb-img"
+                                    src="https://akwam.in/style/assets/images/imdb.png"
+                                    alt="imdb"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="movie-card">
+                        <img
+                            className="movie-card-img"
+                            src="https://i.ebayimg.com/images/g/dAYAAOSwTxhfxK3x/s-l1600.jpg"
+                            alt="movie-img"
+                        />
+                        <div className="movie-card-info">
+                            <div className="rate-wrapper display-flex flex-row">
+                                <Typography
+                                    className="rate-number"
+                                    variant="subtitle1"
+                                    component="div"
+                                >
+                                    8.3/10
+                                </Typography>
+                                <img
+                                    className="imdb-img"
+                                    src="https://akwam.in/style/assets/images/imdb.png"
+                                    alt="imdb"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="movie-card">
+                        <img
+                            className="movie-card-img"
+                            src="https://m.media-amazon.com/images/I/51BtS7ehWTL._AC_.jpg"
+                            alt="movie-img"
+                        />
+                        <div className="movie-card-info">
+                            <div className="rate-wrapper display-flex flex-row">
+                                <Typography
+                                    className="rate-number"
+                                    variant="subtitle1"
+                                    component="div"
+                                >
+                                    8.3/10
+                                </Typography>
+                                <img
+                                    className="imdb-img"
+                                    src="https://akwam.in/style/assets/images/imdb.png"
+                                    alt="imdb"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="movie-card">
+                        <img
+                            className="movie-card-img"
+                            src="https://cdn.europosters.eu/image/750/posters/avengers-endgame-journey-s-end-i73600.jpg"
+                            alt="movie-img"
+                        />
+                        <div className="movie-card-info">
+                            <div className="rate-wrapper display-flex flex-row">
+                                <Typography
+                                    className="rate-number"
+                                    variant="subtitle1"
+                                    component="div"
+                                >
+                                    8.3/10
+                                </Typography>
+                                <img
+                                    className="imdb-img"
+                                    src="https://akwam.in/style/assets/images/imdb.png"
+                                    alt="imdb"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="movie-card">
+                        <img
+                            className="movie-card-img"
+                            src="https://m.media-amazon.com/images/I/81LHuCYlgEL._AC_SY679_.jpg"
+                            alt="movie-img"
+                        />
+                        <div className="movie-card-info">
+                            <div className="rate-wrapper display-flex flex-row">
+                                <Typography
+                                    className="rate-number"
+                                    variant="subtitle1"
+                                    component="div"
+                                >
+                                    8.3/10
+                                </Typography>
+                                <img
+                                    className="imdb-img"
+                                    src="https://akwam.in/style/assets/images/imdb.png"
+                                    alt="imdb"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="movie-card">
+                        <img
+                            className="movie-card-img"
+                            src="https://m.media-amazon.com/images/I/A1PaCX4oXjL._AC_SY679_.jpg"
+                            alt="movie-img"
+                        />
+                        <div className="movie-card-info">
+                            <div className="rate-wrapper display-flex flex-row">
+                                <Typography
+                                    className="rate-number"
+                                    variant="subtitle1"
+                                    component="div"
+                                >
+                                    8.3/10
+                                </Typography>
+                                <img
+                                    className="imdb-img"
+                                    src="https://akwam.in/style/assets/images/imdb.png"
+                                    alt="imdb"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <Footer />
         </section>
     );
