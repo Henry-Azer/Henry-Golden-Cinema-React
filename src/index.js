bce150d242d3a75e5508d1c97fa797511f04e596
Henry-Azer <henaryazer@outlook.com>
2022-03-19 02:04:31 +0200

    Add react-confirm-alert css file

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import "aos/dist/aos.css";
 import "./styles/styles.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import 'react-confirm-alert/src/react-confirm-alert.css'; 
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
